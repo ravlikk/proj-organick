223b4d796acd333a475970f57289baedf1ffabf2
Saharok28 <zozasavchenko1@gmail.com>
2025-07-05 20:02:46 +0300

    ,

--- src/js/auterization.js
+++ src/js/auterization.js
@@ -5,25 +5,6 @@ root.iconUser.addEventListener('click', () => {
   root.modal.classList.add('active');
   root.form.style.display = 'none';
 
-function parseJwt(token) {
-  try {
-
-    const base64Url = token.split('.')[1];
-
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split('')
-        .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-        .join('')
-    );
-    
-    return JSON.parse(jsonPayload);
-  } catch (e) {
-    console.error('Помилка декодування токена:', e.message);
-    return null;
-  }
 }
 
 
