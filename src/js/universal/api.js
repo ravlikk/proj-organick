169ad2009f44b8fc26cda07268e292812221a814
ravlikk <kobetsnazar140510@gmail.com>
2025-07-11 23:39:34 +0300

    .

--- src/js/universal/api.js
+++ src/js/universal/api.js
@@ -7,70 +7,69 @@ export async function registerUser(email, password, url) {
     const res = await axios.post(url, { email, password });
     const data = res.data;
 
-    if (url !== 'https://test-nest-api-iqy9.onrender.com/api/users' && data.token) {
-      localStorage.setItem('token', data.token);
-      localStorage.setItem('isAuthenticated', 'true');
-    } 
+    if (
+      url !== "https://test-nest-api-iqy9.onrender.com/api/users" &&
+      data.token
+    ) {
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("isAuthenticated", "true");
+    }
 
-    return data; 
+    return data;
   } catch (err) {
     if (err.response) {
       throw new Error(err.response.data.message);
     } else {
-      console.error( err.message);
+      console.error(err.message);
       throw err;
     }
   }
 }
-
 
 export async function deleteQuantityOnServer(path, token) {
   console.log(token);
   try {
     const res = await axios.delete(url + path, {
       headers: {
-        Authorization: `Bearer ${token}`
-      }
+        Authorization: `Bearer ${token}`,
+      },
     });
 
-    console.log('Response from server:', res.data);
+    console.log("Response from server:", res.data);
     return res.data;
   } catch (err) {
-    console.error( err.response?.data || err.message);
+    console.error(err.response?.data || err.message);
     return null;
   }
 }
 
-export async function loadCart(path, token ) {
-
+export async function loadCart(path, token) {
   try {
     const res = await axios.get(url + path, {
-    headers: {
-    Authorization: `Bearer ${token}`
-  }
-  
-})
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-    return res.data; 
-    
+    return res.data;
   } catch (err) {
-    console.error( err.response?.data || err.message);
-    return null; 
+    console.error(err.response?.data || err.message);
+    return null;
   }
 }
 
-export async function loadCartDinamic(path, token ) {
+export async function loadCartDinamic(path, token) {
   try {
     const res = await axios.get(url + path, {
-    headers: {
-    Authorization: `Bearer ${token}`
-  }
-})
-  
-    return res.data; 
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return res.data;
   } catch (err) {
-    console.error( err.response?.data || err.message);
-    return null; 
+    console.error(err.response?.data || err.message);
+    return null;
   }
 }
 
@@ -79,19 +78,48 @@ export async function updateQuantityOnServer(path, quantity, token, id) {
     const res = await axios.put(
       url + path,
       {
-        id,         
-        quantity   
+        id,
+        quantity,
       },
       {
         headers: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       }
     );
-    console.log( res.data);
+    console.log(res.data);
     return res.data;
   } catch (err) {
-    console.error( err.response?.data || err.message);
+    console.error(err.response?.data || err.message);
     return null;
+  }
+}
+export async function postToCart() {
+  const token = localStorage.getItem("token");
+
+  try {
+    await axios.post(
+      `${url}/carts`,
+      {
+        products: [
+          {
+            id: 44,
+            quantity: 3,
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+  } catch (error) {
+    if (error.response) {
+      console.error("Статус:", error.response.status);
+      console.error("Дані помилки:", error.response.data);
+    } else {
+      console.error("Помилка:", error.message);
+    }
   }
 }
