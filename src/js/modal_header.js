2f2d6990d544369783a79a5a844bbcb8e34847b9
Saharok28 <zozasavchenko1@gmail.com>
2025-07-20 00:37:47 +0300

    .

--- src/js/modal_header.js
+++ src/js/modal_header.js
@@ -1,47 +1,21 @@
 import { root } from '../js/universal/root';
 
-function openMenu() {
-  root.sidebar.classList.add('active', 'slide-in-right');
-  root.sidebar.classList.remove('slide-out-right');
-
-
-  root.searchBox.classList.remove('search-animate-out');
-  root.searchBox.classList.add('search-animate-in');
-
+function openModal() {
+  root.sidebar.classList.remove('close');
+  root.sidebar.classList.add('open');
+  
 }
 
-function closeMenu() {
-  root.sidebar.classList.remove('slide-in-right');
-  root.sidebar.classList.add('slide-out-right');
-
-  root.searchBox.classList.remove('search-animate-in');
-  root.searchBox.classList.add('search-animate-out');
-
-  setTimeout(() => {
-    root.sidebar.classList.remove('active', 'slide-out-right');
-
-    document.body.classList.remove('modal-open');
-  }, 400);
+function closeModal() {
+  root.sidebar.classList.remove('open');
+  root.sidebar.classList.add('close');
 }
 
-
-root.burger.addEventListener('click', openMenu);
-root.searchBut.addEventListener('click', openMenu);
-root.closeBtn.addEventListener('click', closeMenu);
-document.addEventListener('keydown', (e) => {
- if (e.key === 'Escape' && root.sidebar.classList.contains('active')) {
-    closeMenu(); 
-  }
+root.burger.addEventListener('click', () => {
+  root.sidebar.style.display = 'block';
+openModal();
 });
 
-function toggleNavClass() {
-  const nav = document.querySelector('.header__nav');
-    nav.classList.remove('visible-non-mobile');
-    nav.classList.add('visible-non-mobile');
-}
-
-toggleNavClass();
-
-window.addEventListener('resize', toggleNavClass);
-
-
+root.closeBtn.addEventListener('click', () => {
+closeModal();
+});
