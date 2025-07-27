f912376d9545f9cfd69abecc50ca21ea317dcba3
ravlikk <kobetsnazar140510@gmail.com>
2025-07-27 13:11:09 +0300

    Update getInfToHomepage.js

--- src/js/products/getInfToHomepage.js
+++ src/js/products/getInfToHomepage.js
@@ -3,9 +3,8 @@ import {
   getCategoryCards,
   getProducts,
 } from "../universal/api.js";
-import { createModal } from "./showModal.js";
-import { checkUrlByModal } from "./products/showModal";
-import { validateRoute } from "./universal/validateRoute";
+import { createModal, checkUrlByModal } from "./showModal.js";
+import { validateRoute } from "../universal/validateRoute.js";
 
 createModal();
 getCategoryCards();
